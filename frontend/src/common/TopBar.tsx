9cbb54e0c53caea72b718193fb4a9b821ede2e48
fmuller <florian.muller@thalesgroup.com>
2025-07-23 11:12:52 +0200

    feat: add default file icon

--- frontend/src/common/TopBar.tsx
+++ frontend/src/common/TopBar.tsx
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Box, Container, Fade, Grid2, useTheme, Tooltip, Typography, IconButton, Stack } from "@mui/material";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { Box, Grid2, IconButton, Stack, Tooltip, Typography, useTheme } from "@mui/material";
 import { ReactNode } from "react";
 import InvisibleLink from "../components/InvisibleLink";
 
