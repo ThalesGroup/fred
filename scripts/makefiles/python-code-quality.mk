9ef0c4bc598819c4e5d94a2bea0ac88adcc823f1
fmuller <florian.muller@thalesgroup.com>
2025-07-29 11:40:45 +0200

    chore: add code quality makefile rules to both backend + refactor makefiles with include to share logic

---
+++ scripts/makefiles/python-code-quality.mk
@@ -0,0 +1,31 @@
+##@ Code quality
+
+.PHONY: lint
+lint: ## Run the linter (ruff) 
+	@echo "************ Executing Ruff linter ************"
+	$(UV) run ruff check
+
+.PHONY: lint-fix
+lint-fix: ## Run the linter (ruff) to fix all the auto fixable linter error
+	@echo "************ Executing Ruff linter and apply fix if possible ************"
+	$(UV) run ruff check --fix
+
+.PHONY: format
+format: ## Run the formatter (ruff)
+	@echo "************ Executing Ruff formatter ************"
+	$(UV) run ruff format
+
+.PHONY: sast
+sast: ## Run bandit
+	@echo "************ Executing Ruff formatter with rules B101 (assert_used) and B108 (hardcoded_tmp_directory) ignored ************"
+	$(UV) run bandit -r app -s B101,B108
+
+.PHONY: type-check
+type-check: ## Run type checker (pyright)
+	@echo "************ Executing Pyright type checker ************"
+	$(UV) run pyright
+
+.PHONY: code-quality
+code-quality: ## Run all pre-commit checks
+	@echo "************ Executing pre-commit ************"
+	$(UV) run pre-commit run --all-files
