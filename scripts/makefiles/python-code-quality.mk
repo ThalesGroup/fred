77e930e95511eb8c6706db74d18d827a8598248e
fmuller <florian.muller@thalesgroup.com>
2025-07-29 21:34:24 +0200

    chore: add makefile command to set baselines + group them under .baseline

--- scripts/makefiles/python-code-quality.mk
+++ scripts/makefiles/python-code-quality.mk
@@ -1,7 +1,15 @@
+BASELINE_DIR ?= $(CURDIR)/.baseline
+
+BANDIT_BASELINE_FILE ?= ${BASELINE_DIR}/bandit-baseline.json
+DETECT_SECRET_BASELINE_FILE ?= ${BASELINE_DIR}/detect-secret-baseline.json
+BASEDPYRIGHT_BASELINE_FILE ?= ${BASELINE_DIR}/basedpyright-baseline.json
+
+BANDIT_IGNORED_RULES ?= B101,B108
+
 ##@ Code quality
 
 .PHONY: lint
-lint: ## Run the linter (ruff) 
+lint: ## Run the linter (ruff)
 	@echo "************ Executing Ruff linter ************"
 	$(UV) run ruff check
 
@@ -17,14 +25,53 @@ format: ## Run the formatter (ruff)
 
 .PHONY: sast
 sast: ## Run bandit
-	@echo "************ Executing Ruff formatter with rules B101 (assert_used) and B108 (hardcoded_tmp_directory) ignored ************"
-	$(UV) run bandit -r app -s B101,B108
+	@echo "************ Executing Bandit with rules ${BANDIT_IGNORED_RULES} ingored (B101: assert_used, B108: hardcoded_tmp_directory) ************"
+	$(UV) run bandit \
+		-r ${PY_PACKAGE} \
+		-s ${BANDIT_IGNORED_RULES} \
+		--baseline ${BANDIT_BASELINE_FILE}
+
+detect-secret: ## Run a secret detection tool
+	cd .. && git ls-files -z ${CURDIR} | xargs -0 ${VENV}/bin/detect-secrets-hook --baseline ${DETECT_SECRET_BASELINE_FILE}
 
 .PHONY: type-check
 type-check: ## Run type checker (basedpyright)
 	@echo "************ Executing Basedpyright type checker ************"
-	$(UV) run basedpyright
+	$(UV) run basedpyright --baseline-file ${BASEDPYRIGHT_BASELINE_FILE}
 
 .PHONY: code-quality
-code-quality: lint format sast type-check ## Run all code quality checks
+code-quality: lint format sast detect-secret type-check ## Run all code quality checks
 	@echo "************ All code quality checks completed ************"
+
+# ------------------------------------------------------------
+##@ Code quality - baselines
+
+${BASELINE_DIR}:
+	mkdir -p ${BASELINE_DIR}
+
+baseline-sast: ${BASELINE_DIR} ## Set bandit baseline
+	$(UV) run bandit \
+		-r ${PY_PACKAGE} \
+		-s ${BANDIT_IGNORED_RULES} \
+		-f json \
+		-o ${BANDIT_BASELINE_FILE}
+
+baseline-detect-secret: ${BASELINE_DIR} ## Set detect-secrets baseline
+	cd .. && ${VENV}/bin/detect-secrets scan $(CURDIR) > ${DETECT_SECRET_BASELINE_FILE} 
+
+baseline-type-check: ${BASELINE_DIR} ## Set basedpyright baseline
+	${UV} run basedpyright --baseline-file ${BASEDPYRIGHT_BASELINE_FILE} --writebaseline 
+
+# ------------------------------------------------------------
+# One time setup
+
+# To add the required python dependencies to the uv project
+add-dev-deps:
+	@echo "Adding linter and formater"
+	${UV} add --dev ruff
+	@echo "Adding SAST"
+	${UV} add --dev bandit[baseline]
+	@echo "Adding secret detection"
+	${UV} add --dev detect-secrets
+	@echo "Adding type checker"
+	${UV} add --dev basedpyright
