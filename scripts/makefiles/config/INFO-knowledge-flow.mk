274714bc1da66b9564b29244ea41d065f18a50fe
simcariou <simon.cariou@thalesgroup.com>
2025-08-07 11:53:08 +0200

    impr: Build step with enhanced variabilization (#300)

---
+++ scripts/makefiles/config/INFO-knowledge-flow.mk
@@ -0,0 +1,21 @@
+# Project Metadata
+PROJECT_NAME        ?= knowledge-flow
+PROJECT_SLUG        ?= knowledge_flow_app
+PY_PACKAGE          ?= app
+VERSION             ?= 0.1-dev
+
+# Docker/Registry
+REGISTRY_URL        ?= registry.thalesdigital.io
+REGISTRY_NAMESPACE  ?= tsn/projects/knowledge_flow_app
+DOCKERFILE_PATH     ?= ./dockerfiles/Dockerfile-prod
+DOCKER_CONTEXT      ?= ..
+IMAGE_NAME          ?= $(PROJECT_NAME)
+IMAGE_TAG           ?= $(VERSION)
+IMAGE_FULL          ?= $(REGISTRY_URL)/$(REGISTRY_NAMESPACE)/$(IMAGE_NAME):$(IMAGE_TAG)
+
+# Runtime
+PORT                ?= 8111
+ENV_FILE            ?= .env
+LOG_LEVEL           ?= INFO
+PROJECT_ID          ?= 74648
+HELM_ARCHIVE        ?= ./$(PROJECT_SLUG)-$(VERSION).tgz
