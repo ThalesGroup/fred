d64cbd15597514101d852d806849e636aadca2e8
Simon CARIOU <simon.cariou@thalesgroup.com>
2025-09-02 15:41:56 +0200

    fix: Use user.uid instead of user.username for unique identifier in knowledge-flow resources, tags & document metadata storage

--- knowledge_flow_backend/app/features/resources/service.py
+++ knowledge_flow_backend/app/features/resources/service.py
@@ -1,4 +1,16 @@
-# app/features/resource/service.py
+# Copyright Thales 2025
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from datetime import datetime, timezone
 import logging
@@ -21,9 +33,9 @@ class ResourceService:
         self._resource_store = context.get_resource_store()
 
     def create(self, *, library_tag_id: str, payload: ResourceCreate, user) -> Resource:
-        resource = build_resource_from_create(payload, library_tag_id, user.username)
+        resource = build_resource_from_create(payload, library_tag_id, user.uid)
         res = self._resource_store.create_resource(resource=resource)
-        logger.info(f"[RESOURCES] Created resource {res.id} of kind {res.kind} for user {user.username}")
+        logger.info(f"[RESOURCES] Created resource {res.id} of kind {res.kind} for user {user.uid}")
         return res
 
     def update(self, *, resource_id: str, payload: ResourceUpdate, user) -> Resource:
