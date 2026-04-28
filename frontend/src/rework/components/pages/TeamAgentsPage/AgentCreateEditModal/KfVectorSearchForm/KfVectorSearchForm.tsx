1ae6e921bcaaf70e5e8aed199ef537611581afe0
Dimitri Tombroff <dimitri.tombroff@thalesgroup.com>
2026-04-28 13:33:41 +0200

    1548 fixed production required improvments before go live (#1549)
    * #1548 make reappear the conversation library scoping
    
    * #1548 fixed conversation library selection
    
    * #1548 fixed conversation library selection
    
    * #1548 fixed conversation library selection
    
    ---------
    
    Co-authored-by: Dimitri Tombroff <dimitri.tombroff@gmail.com>

--- frontend/src/rework/components/pages/TeamAgentsPage/AgentCreateEditModal/KfVectorSearchForm/KfVectorSearchForm.tsx
+++ frontend/src/rework/components/pages/TeamAgentsPage/AgentCreateEditModal/KfVectorSearchForm/KfVectorSearchForm.tsx
@@ -46,34 +46,6 @@ export function KfVectorSearchForm({ params, onParamsChange, teamId }: ToolParam
         onChange={(checked) => onParamsChange({ ...params, attach_files: checked })}
       />
 
-      {/* Allow library selection */}
-      <SwitchRow
-        label={t("agentTuning.fields.chat_options_libraries_selection.title")}
-        description={t("agentTuning.fields.chat_options_libraries_selection.description")}
-        checked={Boolean(params.libraries_selection)}
-        onChange={(checked) => onParamsChange({ ...params, libraries_selection: checked })}
-      />
-
-      {/* Directories selection */}
-      <div className={styles.directorySelectionCard}>
-        <div className={styles.directorySelectionLabelSection}>
-          <span className={styles.directorySelectionTitle}>
-            {t("agentTuning.fields.chat_options_libraries_selection.library_selection")}
-          </span>
-          <span className={styles.directorySelectionDescription}>
-            {t("agentTuning.fields.chat_options_libraries_selection.library_selection_description", {
-              agentsNicknameSingular,
-            })}
-          </span>
-        </div>
-
-        <ChatDocumentLibrariesSelectionCard
-          libraryType={"document"}
-          selectedLibrariesIds={params.document_library_tags_ids ?? []}
-          setSelectedLibrariesIds={(ids) => onParamsChange({ ...params, document_library_tags_ids: ids })}
-          teamId={teamId}
-        />
-      </div>
     </div>
   );
 }
