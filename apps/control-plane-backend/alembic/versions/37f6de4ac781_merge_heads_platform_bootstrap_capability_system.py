b49f31eb2f784fd8949038be07a5aa5eb02f1d14
Dimitri Tombroff <dimitri.tombroff@gmail.com>
2026-07-17 11:44:02 +0200

    code quality

--- apps/control-plane-backend/alembic/versions/37f6de4ac781_merge_heads_platform_bootstrap_capability_system.py
+++ apps/control-plane-backend/alembic/versions/37f6de4ac781_merge_heads_platform_bootstrap_capability_system.py
@@ -9,7 +9,7 @@ Create Date: 2026-07-17 05:21:04.073388
 from typing import Sequence, Union
 
 # revision identifiers, used by Alembic.
-revision: str = "37f6de4ac781"
+revision: str = "37f6de4ac781"  # pragma: allowlist secret
 down_revision: Union[str, Sequence[str], None] = ("6e4149d46705", "7fb19a619b0e")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
